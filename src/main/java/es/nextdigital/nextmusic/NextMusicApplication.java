199583ad667efd610c7df2ae59f32a046641acaa
Miguel Albaladejo <miguel.albaladejo@nextdigital.es>
2026-05-12 11:17:22 +0200

    Project scaffolding

---
+++ src/main/java/es/nextdigital/nextmusic/NextMusicApplication.java
@@ -0,0 +1,13 @@
+package es.nextdigital.nextmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextMusicApplication.class, args);
+	}
+
+}
